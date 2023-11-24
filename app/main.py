4d8ed6859a3be45e92900fdd204a076cc0b401a2
Binaya Sharma <binaya.sharma@stackguardian.io>
2023-11-24 22:15:15 +0545

    using ORM instead of raw sql query

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from multiprocessing import synchronize
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -19,21 +20,21 @@ app = FastAPI()
 
 
 
-# while True:
-#     try:
-#         conn = psycopg2.connect(
-#             host='localhost', 
-#             database='python_api', 
-#             user='python_api', 
-#             password='password',
-#             cursor_factory=RealDictCursor)
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost', 
+            database='python_api', 
+            user='python_api', 
+            password='password',
+            cursor_factory=RealDictCursor)
         
-#         cursor = conn.cursor()
-#         print("Database Connection successful!!!")
-#         break
-#     except Exception as e:
-#         print(f"Database connection failed. \n Error: \n {e}")
-#         time.sleep(5)
+        cursor = conn.cursor()
+        print("Database Connection successful!!!")
+        break
+    except Exception as e:
+        print(f"Database connection failed. \n Error: \n {e}")
+        time.sleep(5)
 
 
 # This is used for validation of the body that is being send by the user. 
@@ -41,42 +42,68 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True  # True is the default value.
-    rating: Optional[int] = None # This is the optional field. If user doesn't provide a value it will be None. 
 
 @app.get("/") #@DECORATOR.METHOD-ALLOWED("PATH")
 async def root():  # Path operation Function
     return {"message": "FAST API Learning."}
 
-@app.get("/sql")
-def test_posts(db: Session = Depends(get_db)):
-    return{"Status": "Sucess"}
-
+# @app.get("/posts")
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts""")
+#     posts = cursor.fetchall()
+#     return{"data": posts}
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
     return{"data": posts}
 
+# @app.post("/posts", status_code=status.HTTP_201_CREATED)
+# def create_post(post: Post):
+#     # Converting pydantic model post to a dict and appending into to a my_posts array. 
+#     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
+#                    (post.title, post.content, post.published))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return{
+#         "data": new_post
+#     }
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    # Converting pydantic model post to a dict and appending into to a my_posts array. 
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    
+    #new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+
+    #Retrieve the new_post and store it back to the new_post variable.
+    db.refresh(new_post)
+
     return{
         "data": new_post
     }
 
+
 # {id} is the path parameter. int is the validation, id automatically gets converted into a integer and then passed on. If the value cannot be 
 # converted into a integer the it throws a validation error. 
-@app.get("/posts/{id}")
-def get_post(id: str):
+# @app.get("/posts/{id}")
+# def get_post(id: str):
 
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, 
-                   (str(id),))
-    post = cursor.fetchone()
+#     cursor.execute("""SELECT * FROM posts WHERE id = %s """, 
+#                    (str(id),))
+#     post = cursor.fetchone()
+#     if not post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+#                             detail=f"Post ID {id} not found.")
+#     return {
+#         "data": post
+#    }
+
+@app.get("/posts/{id}")
+def get_post(id: int, db: Session = Depends(get_db)):
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post ID {id} not found.")
@@ -84,18 +111,33 @@ def get_post(id: str):
         "data": post
     }
 
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",
-                   (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+# @app.delete("/posts/{id}")
+# def delete_post(id: int):
+#     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",
+#                    (str(id),))
+#     deleted_post = cursor.fetchone()
+#     conn.commit()
 
-    if deleted_post == None:
+#     if deleted_post == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"Post not found.")
+#     else:
+#         return{"message": "Post was successfully deleted."}
+
+@app.delete("/posts/{id}")
+def delete_post(id: int, db: Session = Depends(get_db)):
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post not found.")
     else:
+        post.delete(synchronize_session=False)
+        db.commit()
         return{"message": "Post was successfully deleted."}
+
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import null, text
 
 from .database import Base
 
@@ -10,3 +11,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
